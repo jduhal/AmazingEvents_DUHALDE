c80b9f7e2be514abd1ba0ba3ed09a72a5c3f0a82
Julio <50839605+jduhal@users.noreply.github.com>
2023-03-15 06:08:46 -0300

    Add files via upload

---
+++ JULIO_DUHALDE_T3_M2/scripts/functions.js
@@ -0,0 +1,99 @@
+// Function for event categories
+const uniqueCategories = (events) => {
+    return events.reduce((acc, curr) => {
+        if (!acc.includes(curr.category)) {
+            acc.push(curr.category);
+        }
+        return acc;
+    }, []);
+};
+  // Function to create the fragment with the checkboxes according to each category
+const createCategoryCheckboxes = (array) => {
+    const fragmentForm = document.createDocumentFragment();
+    const categories = uniqueCategories(array);
+    categories.map(category => {
+        const div = document.createElement('div');
+        div.classList="d-flex flex-wrap form-check form-switch";
+        div.innerHTML=` 
+        <label class="d-inline-flex my-2 mx-2">
+        <input class="form-check-input me-3" value="${category}" 
+        name="categories" role="switch" for="${category}" id="${category}" type="checkbox">${category}
+        </label>`;
+    fragmentForm.appendChild(div);
+    });
+    return fragmentForm;
+}
+// Function that shows the checkboxes in the form
+const showCategoriesInCheckboxes = (array) => {
+    const formCategories = document.getElementsByClassName('formCategories')[0];
+    const fragmentForm = createCategoryCheckboxes(array);
+    let showCategories = formCategories.appendChild(fragmentForm);
+}
+// Function to create event cards
+function createCard(event, ruta) {
+    let div = document.createElement('div');
+    div.classList='card mx-2 my-2';
+    div.innerHTML=`
+        <img src="${event.image}" class="card-img-top" alt="${event.category}">
+        <div class="card-body text-center">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <div class="d-flex justify-content-between">
+                <p class="pt-2">Price: $${event.price}</p>
+                <a href="${ruta}details.html?id=${event._id}" class="btn btn-nav align-self-center go">Go to Details</a>
+            </div>
+        </div>`;
+    return div;
+}
+// Function to loop through events
+function renderCards(elements, container, renderFunction, ruta="./") {
+    let fragmento = document.createDocumentFragment();
+    elements.forEach(element => {
+        const card = renderFunction(element,ruta);
+        fragmento.appendChild(card);
+    });
+    container.appendChild(fragmento);
+}
+// Function not found events
+function displayEvents(events, container, renderFunction, ruta) {
+    if (events.length == 0) {
+        container.innerHTML= `<h2>No results found</h2>`
+    }
+    renderCards(events, container, renderFunction, ruta);
+}
+// Function that creates the Details Cards according to the ID
+function createDetailsCard(event, container) {
+    let div = document.createElement('div');
+    div.classList='row g-0';
+    div.innerHTML=`
+    <div class="col-md-6 d-flex justify-content-center align-items-center">
+        <img src="${event.image}" id="cardImg" class="img-fluid borderRadius " alt="${event.name}">
+    </div>
+    <div class="col-md-6">
+        <div class="card-body">
+            <dl class="card-text">
+                <dt>Name</dt>
+                <dd>${event.name}</dd>
+                <dt>Date</dt>
+                <dd>${event.date}</dd>
+                <dt>Description</dt>
+                <dd>${event.description}</dd>
+                <dt>Category</dt>
+                <dd>${event.category}</dd>
+                <dt>Place</dt>
+                <dd>${event.place}</dd>
+                <dt>Capacity</dt>
+                <dd>${event.capacity}</dd>
+                <dt>${event.assistance ? "Asistence" : "Estimate"}</dt>
+                <dd>${event.assistance ? event.assistance : event.estimate}</dd>
+                <dt>Price</dt>
+                <dd>${event.price}</dd>
+            </dl>
+            <div class="d-flex justify-content-end">
+                <a href="../index.html" class="btn btn-details align-self-center go">Back to Home</a>
+            </div>
+        </div>
+    </div>`;
+    return container.appendChild(div)
+}
+export { createCard, renderCards, displayEvents, showCategoriesInCheckboxes, createDetailsCard, };
---
+++ JULIO_DUHALDE_T3_M2/scripts/index.js
@@ -0,0 +1,36 @@
+import data from "./amazing.js";
+import { displayEvents, createCard, renderCards, showCategoriesInCheckboxes } from './functions.js';
+const divCardsIndex = document.getElementById('cardsIndex');
+const ruta = "./pages/";
+let cards = displayEvents(data.events, divCardsIndex, createCard, ruta);
+let checkboxesCategories = showCategoriesInCheckboxes(data.events);
+const searchInput = document.querySelector('.formSearch > input');
+const searchButton = document.querySelector('.formSearch > button');
+const checkContainer = document.getElementById('formCategories');
+console.log([searchInput]);
+// Search function
+function filterByName(array, name){
+    let filtersArray = array.filter(e => e.name.toLowerCase().includes(name.toLowerCase()))
+    return filtersArray;
+}
+// Function for filtering by categories
+function filterByCategories(array){
+    const checkedValues = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(input => input.value);
+    return checkedValues.length > 0 ? array.filter(e => checkedValues.includes(e.category)) : array;
+}
+// Function for filters to work together
+function ultraFilter(pointer){
+    pointer.preventDefault();
+    divCardsIndex.innerHTML=``
+    let filterArrayName = filterByName(data.events, searchInput.value)
+    let filterAll = filterByCategories(filterArrayName);
+    displayEvents(filterAll, divCardsIndex, createCard, ruta);
+}
+searchInput.addEventListener('input', ultraFilter);
+searchButton.addEventListener('submit', (pointer)=>{
+    ultraFilter(pointer)
+});
+searchButton.addEventListener('onclick', (pointer)=>{
+    ultraFilter(pointer)
+});
+checkContainer.addEventListener('change', ultraFilter);
