4b9d102d39b291cab7642991975c4321d25d6865
Julio <50839605+jduhal@users.noreply.github.com>
2023-03-16 04:30:55 -0300

    Delete JULIO_DUHALDE_T3_M2 directory

--- JULIO_DUHALDE_T3_M2/scripts/details.js
+++
@@ -1,8 +0,0 @@
-import data from "./amazing.js"
-import { createDetailsCard } from "./functions.js";
-const queryString = location.search;
-const param = new URLSearchParams(queryString);
-const eventId = param.get('id');
-const event = data.events.find(event => event._id == eventId);
-let detailsContainer = document.getElementById('cardDetails');
-createDetailsCard(event,detailsContainer);
